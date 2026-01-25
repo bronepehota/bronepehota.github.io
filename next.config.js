1b6bf2c85aa3223825e7f2daad8606e683af9ede
Anton Tuzov <atuzov@naumen.ru>
2026-01-25 14:01:24 +0300

    CUCUMBER

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Base path for GitHub Pages deployment at https://luxor.github.io/bronepehota/
+  basePath: '/bronepehota',
+  assetPrefix: '/bronepehota',
   images: {
     unoptimized: true,
   },
